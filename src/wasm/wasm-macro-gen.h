9b1456ef759985180ffcda510968fc1dbf2727d5
Ben L. Titzer <titzer@google.com>
2015-06-30 11:23:51 +0200

    Fill out wasm-macro-gen.h with new bytecodes.

--- src/wasm/wasm-macro-gen.h
+++ src/wasm/wasm-macro-gen.h
@@ -92,7 +92,7 @@
 #define WASM_BINOP(opcode, x, y) static_cast<byte>(opcode), x, y
 
 //------------------------------------------------------------------------------
-// Integer binops.
+// Int32 operations
 //------------------------------------------------------------------------------
 #define WASM_INT32_ADD(x, y) kExprInt32Add, x, y
 #define WASM_INT32_SUB(x, y) kExprInt32Sub, x, y
@@ -112,29 +112,77 @@
 #define WASM_INT32_SLE(x, y) kExprInt32Sle, x, y
 #define WASM_INT32_ULT(x, y) kExprInt32Ult, x, y
 #define WASM_INT32_ULE(x, y) kExprInt32Ule, x, y
+#define WASM_INT32_CLZ(x) kExprInt32Clz, x
+#define WASM_INT32_CTZ(x) kExprInt32Ctz, x
+#define WASM_INT32_POPCNT(x) kExprInt32PopCnt, x
 
 //------------------------------------------------------------------------------
-// Float64 binops.
+// Int64 operations
 //------------------------------------------------------------------------------
-#define WASM_FLOAT64_ADD(x, y) kExprFloat64Add, x, y
-#define WASM_FLOAT64_SUB(x, y) kExprFloat64Sub, x, y
-#define WASM_FLOAT64_MUL(x, y) kExprFloat64Mul, x, y
-#define WASM_FLOAT64_DIV(x, y) kExprFloat64Div, x, y
-#define WASM_FLOAT64_EQ(x, y) kExprFloat64Eq, x, y
-#define WASM_FLOAT64_LT(x, y) kExprFloat64Lt, x, y
-#define WASM_FLOAT64_LE(x, y) kExprFloat64Le, x, y
-
+#define WASM_INT64_ADD(x, y) kExprInt64Add, x, y
+#define WASM_INT64_SUB(x, y) kExprInt64Sub, x, y
+#define WASM_INT64_MUL(x, y) kExprInt64Mul, x, y
+#define WASM_INT64_SDIV(x, y) kExprInt64SDiv, x, y
+#define WASM_INT64_UDIV(x, y) kExprInt64UDiv, x, y
+#define WASM_INT64_SREM(x, y) kExprInt64SRem, x, y
+#define WASM_INT64_UREM(x, y) kExprInt64URem, x, y
+#define WASM_INT64_AND(x, y) kExprInt64And, x, y
+#define WASM_INT64_IOR(x, y) kExprInt64Ior, x, y
+#define WASM_INT64_XOR(x, y) kExprInt64Xor, x, y
+#define WASM_INT64_SHL(x, y) kExprInt64Shl, x, y
+#define WASM_INT64_SHR(x, y) kExprInt64Shr, x, y
+#define WASM_INT64_SAR(x, y) kExprInt64Sar, x, y
+#define WASM_INT64_EQ(x, y) kExprInt64Eq, x, y
+#define WASM_INT64_SLT(x, y) kExprInt64Slt, x, y
+#define WASM_INT64_SLE(x, y) kExprInt64Sle, x, y
+#define WASM_INT64_ULT(x, y) kExprInt64Ult, x, y
+#define WASM_INT64_ULE(x, y) kExprInt64Ule, x, y
+#define WASM_INT64_CLZ(x) kExprInt64Clz, x
+#define WASM_INT64_CTZ(x) kExprInt64Ctz, x
+#define WASM_INT64_POPCNT(x) kExprInt64PopCnt, x
 
 //------------------------------------------------------------------------------
-// Float32 binops.
+// Float32 operations
 //------------------------------------------------------------------------------
 #define WASM_FLOAT32_ADD(x, y) kExprFloat32Add, x, y
 #define WASM_FLOAT32_SUB(x, y) kExprFloat32Sub, x, y
 #define WASM_FLOAT32_MUL(x, y) kExprFloat32Mul, x, y
 #define WASM_FLOAT32_DIV(x, y) kExprFloat32Div, x, y
+#define WASM_FLOAT32_MIN(x, y) kExprFloat32Min, x, y
+#define WASM_FLOAT32_MAX(x, y) kExprFloat32Max, x, y
+#define WASM_FLOAT32_ABS(x) kExprFloat32Abs, x
+#define WASM_FLOAT32_NEG(x) kExprFloat32Neg, x
+#define WASM_FLOAT32_COPYSIGN(x) kExprFloat32CopySign, x
+#define WASM_FLOAT32_CEIL(x) kExprFloat32Ceil, x
+#define WASM_FLOAT32_FLOOR(x) kExprFloat32Floor, x
+#define WASM_FLOAT32_TRUNC(x) kExprFloat32Trunc, x
+#define WASM_FLOAT32_NEARESTINT(x) kExprFloat32NearestInt, x
+#define WASM_FLOAT32_SQRT(x) kExprFloat32Sqrt, x
 #define WASM_FLOAT32_EQ(x, y) kExprFloat32Eq, x, y
 #define WASM_FLOAT32_LT(x, y) kExprFloat32Lt, x, y
 #define WASM_FLOAT32_LE(x, y) kExprFloat32Le, x, y
+
+
+//------------------------------------------------------------------------------
+// Float64 operations
+//------------------------------------------------------------------------------
+#define WASM_FLOAT64_ADD(x, y) kExprFloat64Add, x, y
+#define WASM_FLOAT64_SUB(x, y) kExprFloat64Sub, x, y
+#define WASM_FLOAT64_MUL(x, y) kExprFloat64Mul, x, y
+#define WASM_FLOAT64_DIV(x, y) kExprFloat64Div, x, y
+#define WASM_FLOAT64_MIN(x, y) kExprFloat64Min, x, y
+#define WASM_FLOAT64_MAX(x, y) kExprFloat64Max, x, y
+#define WASM_FLOAT64_ABS(x) kExprFloat64Abs, x
+#define WASM_FLOAT64_NEG(x) kExprFloat64Neg, x
+#define WASM_FLOAT64_COPYSIGN(x) kExprFloat64CopySign, x
+#define WASM_FLOAT64_CEIL(x) kExprFloat64Ceil, x
+#define WASM_FLOAT64_FLOOR(x) kExprFloat64Floor, x
+#define WASM_FLOAT64_TRUNC(x) kExprFloat64Trunc, x
+#define WASM_FLOAT64_NEARESTINT(x) kExprFloat64NearestInt, x
+#define WASM_FLOAT64_SQRT(x) kExprFloat64Sqrt, x
+#define WASM_FLOAT64_EQ(x, y) kExprFloat64Eq, x, y
+#define WASM_FLOAT64_LT(x, y) kExprFloat64Lt, x, y
+#define WASM_FLOAT64_LE(x, y) kExprFloat64Le, x, y
 
 
 //------------------------------------------------------------------------------
@@ -144,11 +192,24 @@
 #define WASM_INT32_SCONVERT_FLOAT64(x) kExprInt32SConvertFloat64, x
 #define WASM_INT32_UCONVERT_FLOAT32(x) kExprInt32UConvertFloat32, x
 #define WASM_INT32_UCONVERT_FLOAT64(x) kExprInt32UConvertFloat64, x
-#define WASM_FLOAT64_SCONVERT_INT32(x) kExprFloat64SConvertInt32, x
-#define WASM_FLOAT64_UCONVERT_INT32(x) kExprFloat64UConvertInt32, x
-#define WASM_FLOAT64_CONVERT_FLOAT32(x) kExprFloat64ConvertFloat32, x
+#define WASM_INT32_CONVERT_INT64(x) kExprInt32ConvertInt64, x
+#define WASM_INT64_SCONVERT_FLOAT32(x) kExprInt64SConvertFloat32, x
+#define WASM_INT64_SCONVERT_FLOAT64(x) kExprInt64SConvertFloat64, x
+#define WASM_INT64_UCONVERT_FLOAT32(x) kExprInt64UConvertFloat32, x
+#define WASM_INT64_UCONVERT_FLOAT64(x) kExprInt64UConvertFloat64, x
+#define WASM_INT64_SCONVERT_INT32(x) kExprInt64SConvertInt32, x
+#define WASM_INT64_UCONVERT_INT32(x) kExprInt64UConvertInt32, x
 #define WASM_FLOAT32_SCONVERT_INT32(x) kExprFloat32SConvertInt32, x
 #define WASM_FLOAT32_UCONVERT_INT32(x) kExprFloat32UConvertInt32, x
+#define WASM_FLOAT32_SCONVERT_INT64(x) kExprFloat32SConvertInt64, x
+#define WASM_FLOAT32_UCONVERT_INT64(x) kExprFloat32UConvertInt64, x
 #define WASM_FLOAT32_CONVERT_FLOAT64(x) kExprFloat32ConvertFloat64, x
+#define WASM_FLOAT32_REINTERPRET_INT32(x) kExprFloat32ReinterpretInt32, x
+#define WASM_FLOAT64_SCONVERT_INT32(x) kExprFloat64SConvertInt32, x
+#define WASM_FLOAT64_UCONVERT_INT32(x) kExprFloat64UConvertInt32, x
+#define WASM_FLOAT64_SCONVERT_INT64(x) kExprFloat64SConvertInt64, x
+#define WASM_FLOAT64_UCONVERT_INT64(x) kExprFloat64UConvertInt64, x
+#define WASM_FLOAT64_CONVERT_FLOAT32(x) kExprFloat64ConvertFloat32, x
+#define WASM_FLOAT64_REINTERPRET_INT64(x) kExprFloat64ReinterpretInt64, x
 
 #endif  // V8_WASM_MACRO_GEN_H_
