eff361d3129f4e38d7b904e87f488f192638efc6
Brad Nelson <bradnelson@chromium.org>
2015-10-08 15:40:36 -0700

    Switch travis build to optdebug.

--- travis/build-and-test.sh
+++ travis/build-and-test.sh
@@ -10,15 +10,15 @@ cd $(dirname ${BASH_SOURCE[0]})/..
 cd v8/v8
 
 echo "==== BUILDING V8 ===="
-make x64.debug wasm=on werror=no
+make x64.optdebug wasm=on werror=no
 
 echo "==== unittests ===="
-./out/x64.debug/unittests "--gtest_filter=Wasm*"
+./out/x64.optdebug/unittests "--gtest_filter=Wasm*"
 
 echo "==== cctest/test-run-wasm* ===="
 ./tools/run-tests.py \
-  --no-presubmit --mode debug --arch x64 cctest/test-run-wasm*
+  --no-presubmit --mode optdebug --arch x64 cctest/test-run-wasm*
 
 echo "==== mjsunit/wasm/* ===="
 ./tools/run-tests.py \
-  --no-presubmit --mode debug --arch x64 mjsunit/wasm/*
+  --no-presubmit --mode optdebug --arch x64 mjsunit/wasm/*
